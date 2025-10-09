97c51ccc4cffa71f35da528dbdde9fbed7c3fcd5
owenwinecoff <winecoffowen@icloud.com>
2025-10-09 16:03:06 -0500

    Edit map bounds, marker functionality

--- maplibre-base.js
+++ maplibre-base.js
@@ -2,9 +2,17 @@
 var map = new maplibregl.Map({
     container: 'map',
     style: 'https://demotiles.maplibre.org/style.json',
-    center: [-98, 39],
-    zoom: 4
+    center: [-98, 38.8],
+    zoom: 3.8,
+    maxBounds: [
+        [-125, 24], // Southwest coordinates (longitude, latitude)
+        [-66, 50]   // Northeast coordinates
+    ]
 });
+
+let marker = new maplibregl.Marker();
+marker.setLngLat([-93.7, 45.3]);
+marker.addTo(map);
 
 // Add navigation controls
 map.addControl(new maplibregl.NavigationControl());
